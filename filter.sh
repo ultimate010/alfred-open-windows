30ed1c46a2bdabc41ebd579f1300918baa975ec4
ultimate010 <ultimate010@gmail.com>
2015-08-10 19:42:50 +0800

    Add support to switch between applications

--- filter.sh
+++ filter.sh
@@ -11,7 +11,7 @@ osascript -sh windows.ascr | tr "\r" "\n" | while read LINE; do
     if [ "$FILTER" = "" ]; then
         continue
     else
-        if echo $LINE | grep -sq $FILTER; then
+        if echo $LINE | grep -sqi $FILTER; then
           true
         else
           continue
@@ -21,6 +21,30 @@ osascript -sh windows.ascr | tr "\r" "\n" | while read LINE; do
     echo "  <title>${LINE}</title>"
     echo "  <subtitle>Switch to this window</subtitle>"
     echo "  <icon type=\"fileicon\">/${APPLICATION}</icon>"
+    echo "</item>"
+done
+
+osascript -sh applications.ascr | tr "\r" "\n" | cut -f 2- -d : |  tr ":" "/" | while read LINE; do
+    if [ "$LINE" = "" ]; then
+        continue
+    fi
+    if [ "$FILTER" = "" ]; then
+        continue
+    else
+        if echo $LINE | grep -sqi $FILTER; then
+            if echo $LINE | grep -sq 'app' ; then
+                true
+            else
+                continue
+            fi
+        else
+            continue
+        fi
+    fi
+    echo "<item arg=\"/${LINE}\">"
+    echo "  <title>`echo ${LINE} | awk -F '/' '{print $(NF - 1)}' `</title>"
+    echo "  <subtitle>Switch to this window</subtitle>"
+    echo "  <icon type=\"fileicon\">/${LINE}</icon>"
     echo "</item>"
 done
 
