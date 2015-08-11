43a0a6a28b0d8aa1d781ed8bb08cb242d06d4be4
ultimate010 <ultimate010@gmail.com>
2015-08-11 22:09:19 +0800

    Only swith application

--- filter.sh
+++ filter.sh
@@ -2,27 +2,27 @@
 FILTER="$1"
 if [ -z "$FILTER" ]; then FILTER=.;fi
 echo '<?xml version="1.0"?><items>'
-APPLICATION=$(osascript -sh application.ascr | cut -f 2- -d : | cut -f -2 -d : | tr ":" "/")
+# APPLICATION=$(osascript -sh application.ascr | cut -f 2- -d : | cut -f -2 -d : | tr ":" "/")
 
-osascript -sh windows.ascr | tr "\r" "\n" | while read LINE; do
-    if [ "$LINE" = "" ]; then
-        continue
-    fi
-    if [ "$FILTER" = "" ]; then
-        continue
-    else
-        if echo $LINE | grep -sqi $FILTER; then
-          true
-        else
-          continue
-        fi
-    fi
-    echo "<item arg=\"${LINE}\">"
-    echo "  <title>${LINE}</title>"
-    echo "  <subtitle>Switch to this window</subtitle>"
-    echo "  <icon type=\"fileicon\">/${APPLICATION}</icon>"
-    echo "</item>"
-done
+# osascript -sh windows.ascr | tr "\r" "\n" | while read LINE; do
+#     if [ "$LINE" = "" ]; then
+#         continue
+#     fi
+#     if [ "$FILTER" = "" ]; then
+#         continue
+#     else
+#         if echo $LINE | grep -sqi $FILTER; then
+#           true
+#         else
+#           continue
+#         fi
+#     fi
+#     echo "<item arg=\"${LINE}\">"
+#     echo "  <title>${LINE}</title>"
+#     echo "  <subtitle>Switch to this window</subtitle>"
+#     echo "  <icon type=\"fileicon\">/${APPLICATION}</icon>"
+#     echo "</item>"
+# done
 
 osascript -sh applications.ascr | tr "\r" "\n" | cut -f 2- -d : |  tr ":" "/" | while read LINE; do
     if [ "$LINE" = "" ]; then
